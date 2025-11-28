dd702ec48651e5008f1ce480eb72696ad7cec322
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-28 18:36:57 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    avatar = db.Column(db.String(200), default="static/avatar/default.png")
+    role = db.Column(db.String(20), default="user")
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(200))
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    convo_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
+    sender = db.Column(db.String(20))
+    text = db.Column(db.Text)
